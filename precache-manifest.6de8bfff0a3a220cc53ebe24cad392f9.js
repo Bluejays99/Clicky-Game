e622ece35da092929c786c4c0c6d60017b475601
Chris Minassian <cminassian@live.com>
2019-10-26 15:13:19 -0700

    Updates

---
+++ precache-manifest.6de8bfff0a3a220cc53ebe24cad392f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914bf22fa7af894694f9311e7f91a699",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "e9b4ecc6d1dc1028585f",
+    "url": "/Clicky-Game/static/css/main.a64a7c44.chunk.css"
+  },
+  {
+    "revision": "7fc024a4fe7cc60fae2a",
+    "url": "/Clicky-Game/static/js/2.b19a2de5.chunk.js"
+  },
+  {
+    "revision": "e9b4ecc6d1dc1028585f",
+    "url": "/Clicky-Game/static/js/main.0e548a3a.chunk.js"
+  },
+  {
+    "revision": "eabcff367a27d49b89d0",
+    "url": "/Clicky-Game/static/js/runtime-main.ca802268.js"
+  }
+]);
